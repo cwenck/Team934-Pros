6edba6bd7bdb3a45d9b8b49ce346c515bbda131a
Connor Wenck <cwenck@me.com>
2014-11-06 16:14:10 -0500

    renamed project folders

---
+++ Terminator/include/general_functions.h
@@ -0,0 +1,226 @@
+/*
+ * general_functions.h
+ *
+ *  Created on: Oct 18, 2014
+ *      Author: guywenck
+ */
+
+#ifndef GENERAL_FUNCTIONS_H_
+#define GENERAL_FUNCTIONS_H_
+
+#include "main.h"
+
+////////////
+void handleAllInput();
+///////////
+
+//////////
+//Motors//
+//////////
+typedef struct {
+	unsigned char port;
+	unsigned char imeAddress;
+	bool reversed;
+} Motor;
+
+//vars
+extern unsigned char connectedIntegratedMotorEncoders;
+
+extern Motor frontLeftWheel;
+extern Motor frontRightWheel;
+extern Motor backLeftWheel;
+extern Motor backRightWheel;
+
+extern Motor frontLeftLift;
+extern Motor backLeftLift;
+extern Motor frontRightLift;
+extern Motor backRightLift;
+
+//functions
+void setMotorPower(Motor motor, int speed);
+
+Motor createMotor(unsigned char port, bool reversed);
+Motor createMotorWithIME(unsigned char port, unsigned char imeAddress,
+		bool reversed);
+
+/////////
+//Drive//
+/////////
+
+typedef enum {
+	same, towards, away
+} WheelDirection;
+
+typedef enum {
+	left, right, forward, backward
+} Direction;
+
+//vars
+extern const int DRIVE_THRESHOLD;
+
+//functions
+void setRightMotorSpeed(int speed, WheelDirection dir);
+void setLeftMotorSpeed(int speed, WheelDirection dir);
+
+void strafe(int speed, Direction dir);
+void drive(int speed, Direction dir);
+
+void handleDriveInput();
+void handleStrafingInput();
+
+void handleDriveOrStrafing();
+
+////////
+//Lift//
+////////
+
+//vars
+extern const short liftHighPower;
+extern const short liftLowPower;
+
+//functions
+void handleLiftInput();
+void setLiftPower(int speed);
+
+////////////
+//Controls//
+////////////
+
+extern unsigned char connectedJoysticks;
+
+typedef struct {
+	unsigned char btn;
+	unsigned char channel;
+	bool onPartnerJoystick;
+
+} JoyInput;
+
+//vars
+extern JoyInput liftUp;
+extern JoyInput liftDown;
+
+extern JoyInput forward_backward_drive;
+extern JoyInput left_right_drive;
+extern JoyInput forward_backward_strafe;
+extern JoyInput left_right_strafe;
+
+//functions
+int readJoystick(JoyInput button);
+unsigned char getNumConnectedJoysticks();
+
+JoyInput createButton(unsigned char channel, unsigned char btn);
+JoyInput createButtonOnPartnerJoystick(unsigned char channel, unsigned char btn);
+JoyInput createAxis(unsigned char channel);
+JoyInput createAxisOnPartnerJoystick(unsigned char channel);
+
+///////////
+//Sensors//
+///////////
+
+typedef struct {
+	unsigned char port;
+} Bumper;
+
+typedef struct {
+	unsigned char port;
+} LimitSwitch;
+
+typedef enum {
+	IntegratedMotorEncoder,	//IC2
+	QuadratureEncoder,		//?
+	Sonar,					//Digital
+	Line,					//?
+	Light,					//?
+	Push_Button,			//Digital
+	Limit_Switch,			//Digital
+	Potentiometer,			//Analog
+	Gyroscope,				//Analog
+	Accelerometer			//Analog
+} SensorType;
+
+typedef enum {
+	Digital_1,
+	Digital_2,
+	Digital_3,
+	Digital_4,
+	Digital_5,
+	Digital_6,
+	Digital_7,
+	Digital_8,
+	Digital_9,
+	Digital_10,
+	Digital_11,
+	Digital_12,
+	Analog_1,
+	Analog_2,
+	Analog_3,
+	Analog_4,
+	Analog_5,
+	Analog_6,
+	Analog_7,
+	Analog_8,
+	IME_1,
+	IME_2,
+	IME_3,
+	IME_4,
+	IME_5,
+	IME_6,
+	IME_7,
+	IME_8,
+} SensorPort;
+
+typedef struct {
+	SensorType type;
+	SensorPort port_1;
+
+	//Only used for Sonar and Quadrature encoders
+	//order does not matter for the quadrature encoder,
+	//it only reverses the direction of the rotation
+	//This should be NULL if it is not one of those two sensor types.
+	SensorPort port_2;
+} Sensor;
+
+//vars
+extern Encoder liftEncoder;
+
+//functions
+Bumper bumperInit(unsigned char port);
+bool bumperPressed(Bumper bumper);
+
+LimitSwitch limitSwitchInit(unsigned char port);
+bool limitSwitchPressed(LimitSwitch limitSwitch);
+
+Sensor createSensor(SensorType, SensorPort port_1, SensorPort port_2);
+int getSensorValue(Sensor sensor);
+int sensorGet(Sensor sensor);	//alias for getSensorValue(Sensor sensor);
+
+///////
+//PID//
+///////
+
+typedef struct {
+	float kp;
+	float ki;
+	float kd;
+	float error;
+	float last_error;
+	float target;
+	float position;
+	float integral;
+	float integral_min;
+	float integral_max;
+	float derivative;
+	void (*setMotorSpeedFunction)(int);
+	Sensor sensor;
+} PIDController;
+
+/////////
+//Other//
+/////////
+
+//returns the main battery power in volts
+float getMainBatteryPower();
+//print the main batter power in volts
+void printMainBatteryPower();
+
+#endif /*GENERAL_FUNCTIONS_H_*/
---
+++ Terminator/src/general_functions.c
@@ -0,0 +1,345 @@
+#include "main.h"
+
+/////////////
+//Variables//
+/////////////
+
+//Lift liftEncoder
+Encoder liftEncoder;
+
+//Init Drive Motors
+Motor frontLeftWheel;
+Motor frontRightWheel;
+Motor backLeftWheel;
+Motor backRightWheel;
+
+//Init Lift Motors
+Motor frontLeftLift;
+Motor backLeftLift;
+Motor frontRightLift;
+Motor backRightLift;
+
+//Init Controller Buttons
+JoyInput liftUp;
+JoyInput liftDown;
+
+JoyInput forward_backward_drive;
+JoyInput left_right_drive;
+JoyInput forward_backward_strafe;
+JoyInput left_right_strafe;
+
+void handleAllInput() {
+	handleDriveOrStrafing();
+	handleLiftInput();
+}
+
+////////////
+//Joystick//
+////////////
+
+JoyInput createButton(unsigned char channel, unsigned char btn) {
+	JoyInput button;
+	button.channel = channel;
+	button.btn = btn;
+	button.onPartnerJoystick = false;
+	return button;
+}
+
+JoyInput createButtonOnPartnerJoystick(unsigned char channel, unsigned char btn) {
+	JoyInput button;
+	button.channel = channel;
+	button.btn = btn;
+	button.onPartnerJoystick = true;
+	return button;
+}
+
+JoyInput createAxis(unsigned char channel) {
+	JoyInput button;
+	button.channel = channel;
+	button.btn = NULL;
+	button.onPartnerJoystick = false;
+	return button;
+}
+
+JoyInput createAxisOnPartnerJoystick(unsigned char channel) {
+	JoyInput button;
+	button.channel = channel;
+	button.btn = NULL;
+	button.onPartnerJoystick = true;
+	return button;
+}
+
+unsigned char getNumConnectedJoysticks() {
+	unsigned char joysticksConnected = 0;
+	if (isJoystickConnected(1)) {
+		joysticksConnected++;
+	}
+	if (isJoystickConnected(2)) {
+		joysticksConnected++;
+	}
+	return joysticksConnected;
+}
+
+//If it is a button a 1 is returned for true and 0 for false
+//otherwise a value between -127 and 127 is returned for the particular axis
+int readJoystick(JoyInput button) {
+	unsigned char joy = 1;
+	if (button.onPartnerJoystick) {
+		joy = 2;
+	}
+	if (button.channel == 1 || button.channel == 2 || button.channel == 3
+			|| button.channel == 4) {
+		return joystickGetAnalog(joy, button.channel);
+	} else {
+		if (joystickGetDigital(joy, button.channel, button.btn)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+}
+
+//////////
+//Motors//
+//////////
+
+void setMotorPower(Motor motor, int speed) {
+	if (motor.reversed) {
+		speed = -speed;
+	}
+	motorSet(motor.port, speed);
+}
+
+Motor createMotor(unsigned char port, bool reversed) {
+	Motor motor;
+	motor.port = port;
+	motor.reversed = reversed;
+	motor.imeAddress = NULL;
+	return motor;
+}
+
+//Integrated motor encoders closest to the cortex in the chain get assigned an address of 0
+//encoders futher down the line get an address that is incremented down the line
+//so the next further one from the cortex would be 1 then 2 then 3 etc.
+Motor createMotorWithIME(unsigned char port, unsigned char imeAddress,
+		bool reversed) {
+	Motor motor;
+	motor.port = port;
+	motor.reversed = reversed;
+	motor.imeAddress = imeAddress;
+	return motor;
+}
+
+////////
+//Lift//
+////////
+
+const short liftHighPower = 127;
+const short liftLowPower = 80;
+
+void setLiftPower(int speed) {
+	setMotorPower(frontLeftLift, speed);
+	setMotorPower(backLeftLift, speed);
+	setMotorPower(frontRightLift, speed);
+	setMotorPower(backRightLift, speed);
+}
+
+void handleLiftInput() {
+	if (readJoystick(liftUp) == 1) {
+		setLiftPower(liftHighPower);
+	} else if (readJoystick(liftDown) == 1) {
+		setLiftPower(-liftHighPower);
+	} else {
+		setLiftPower(0);
+	}
+}
+
+/////////
+//Drive//
+/////////
+
+const int DRIVE_THRESHOLD = 5;
+
+/*
+ * A positive value for speed is forwards
+ * A negetive value for speed is backwards (only works if the direction is same)
+ */
+void setRightMotorSpeed(int speed, WheelDirection dir) {
+	if (dir == same) {
+		setMotorPower(frontRightWheel, speed);
+		setMotorPower(backRightWheel, speed);
+	} else if (dir == towards) {
+		speed = abs(speed);
+		setMotorPower(frontRightWheel, -speed);
+		setMotorPower(backRightWheel, speed);
+	} else if (dir == away) {
+		speed = abs(speed);
+		setMotorPower(frontRightWheel, speed);
+		setMotorPower(backRightWheel, -speed);
+	}
+}
+
+/*
+ * A positive value for speed is forwards
+ * A negetive value for speed is backwards (only works if the direction is same)
+ */
+void setLeftMotorSpeed(int speed, WheelDirection dir) {
+	if (dir == same) {
+		setMotorPower(frontLeftWheel, speed);
+		setMotorPower(backLeftWheel, speed);
+	} else if (dir == towards) {
+		speed = abs(speed);
+		setMotorPower(frontLeftWheel, -speed);
+		setMotorPower(backLeftWheel, speed);
+	} else if (dir == away) {
+		speed = abs(speed);
+		setMotorPower(frontLeftWheel, speed);
+		setMotorPower(backLeftWheel, -speed);
+	}
+}
+
+/*
+ * Strafe left and right at a particular speed
+ * or move forawrd or backward
+ */
+void strafe(int speed, Direction dir) {
+	speed = abs(speed);
+	if (dir == left) {
+		setLeftMotorSpeed(speed, towards);
+		setRightMotorSpeed(speed, away);
+	} else if (dir == right) {
+		setLeftMotorSpeed(speed, away);
+		setRightMotorSpeed(speed, towards);
+	} else if (dir == forward) {
+		setLeftMotorSpeed(speed, same);
+		setRightMotorSpeed(speed, same);
+	} else if (dir == backward) {
+		setLeftMotorSpeed(-speed, same);
+		setRightMotorSpeed(-speed, same);
+	}
+}
+
+/*
+ * Drive forward or backward or turn left or turn right at a particular speed
+ */
+
+void drive(int speed, Direction dir) {
+	speed = abs(speed);
+	if (dir == left) {
+		setLeftMotorSpeed(-speed, same);
+		setRightMotorSpeed(speed, same);
+	} else if (dir == right) {
+		setLeftMotorSpeed(speed, same);
+		setRightMotorSpeed(-speed, same);
+	} else if (dir == forward) {
+		setLeftMotorSpeed(speed, same);
+		setRightMotorSpeed(speed, same);
+	} else if (dir == backward) {
+		setLeftMotorSpeed(-speed, same);
+		setRightMotorSpeed(-speed, same);
+	}
+}
+
+void handleDriveInput() {
+	int y = readJoystick(forward_backward_drive);
+	int x = readJoystick(left_right_drive);
+
+	if (abs(y) > abs(x)) {
+		setLeftMotorSpeed(y, same);
+		setRightMotorSpeed(y, same);
+	} else if (abs(x) >= abs(y)) {
+		setLeftMotorSpeed(x, same);
+		setRightMotorSpeed(-x, same);
+	}
+}
+
+void handleStrafingInput() {
+	if (abs(
+			readJoystick(forward_backward_strafe)
+					> abs(readJoystick(left_right_strafe)))) {
+		if (readJoystick(forward_backward_strafe) > 0) {
+			strafe(readJoystick(forward_backward_strafe), forward);
+		} else {
+			strafe(readJoystick(forward_backward_strafe), backward);
+		}
+	} else {
+		if (readJoystick(left_right_strafe) > 0) {
+			strafe(readJoystick(left_right_strafe), right);
+		} else {
+			strafe(readJoystick(left_right_strafe), left);
+		}
+	}
+}
+
+//Decides whether to drive or strafe
+void handleDriveOrStrafing() {
+	if (abs(readJoystick(forward_backward_drive)) <= DRIVE_THRESHOLD
+			&& abs(readJoystick(left_right_drive)) <= DRIVE_THRESHOLD) {
+		handleDriveInput();
+	} else {
+		handleStrafingInput();
+	}
+}
+
+///////////
+//Sensors//
+///////////
+
+//Init bumpers in initializeIO()
+Bumper bumperInit(unsigned char port) {
+	Bumper bumper;
+	bumper.port = port;
+	pinMode(bumper.port, INPUT);
+	return bumper;
+}
+
+// digitalRead() returns LOW if Pressed or HIGH if released
+// the function returns true if the bumper is pressed
+bool bumperPressed(Bumper bumper) {
+	if (digitalRead(bumper.port) == LOW) {
+		return true;
+	}
+	return false;
+}
+
+// Init limit switches in initializeIO()
+LimitSwitch limitSwitchInit(unsigned char port) {
+	LimitSwitch limitSwitch;
+	limitSwitch.port = port;
+	pinMode(limitSwitch.port, INPUT);
+	return limitSwitch;
+}
+
+// digitalRead() returns LOW if Pressed or HIGH if released
+// the function returns true if the bumper is pressed
+bool limitSwitchPressed(LimitSwitch limitSwitch) {
+	if (digitalRead(limitSwitch.port) == LOW) {
+		return true;
+	}
+	return false;
+}
+
+int getSensorValue(Sensor sensor){
+
+}
+
+int sensorGet(Sensor sensor){
+	return getSensorValue(sensor);
+}
+
+///////
+//PID//
+///////
+
+
+/////////
+//Other//
+/////////
+
+float getMainBatteryPower() {
+	return ((float) powerLevelMain()) / 1000.0f;
+}
+
+void printMainBatteyPower() {
+	printf("Main Battery: %.2fV", getMainBatteryPower());
+}
